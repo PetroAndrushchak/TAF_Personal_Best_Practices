b612d9bb8a402816e7035571aaaa44a77842120e
Petro AndrushchakWork <pandr@softserveinc.com>
2021-12-26 19:14:03 +0200

    test

--- web-ui-tests-junit5/src/test/java/tests/junit5/C7777_LogInTest.java
+++ web-ui-tests-junit5/src/test/java/tests/junit5/C7777_LogInTest.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@DisplayName("Log in")
+import static io.qameta.allure.Allure.step;
+
+@DisplayName("Company")
 public class C7777_LogInTest extends JUnit5BaseTest {
 
     @Autowired
@@ -24,7 +26,8 @@ public class C7777_LogInTest extends JUnit5BaseTest {
     @Owner("Petro Andrushcahk")
     @Severity(SeverityLevel.NORMAL)
     @TmsLink("C777")
-    @Tag("ui") @Tag("regression")
+    @Tag("ui")
+    @Tag("regression")
     @Test
     public void agentEmailWhenUserLeftListingComment() {
         Agent agent = UserRegistry.getAgent();
@@ -35,5 +38,21 @@ public class C7777_LogInTest extends JUnit5BaseTest {
 
     }
 
+    @Epic("Companies")
+    @Feature("Team Member")
+    @TmsLink("TC-177")
+    @Link("IRP-496")
+    @Tag("ui")
+    @Tag("regression")
+    @DisplayName("Team Member see notice \"Company Deactivated\"  when the Company is inactive.")
+    @Test
+    public void teamMemberSeeCompanyIsDeactivatedNoticeWhenCompanyIsInActive() {
+        step("Create a company: TAF_COMPANY_1");
+        step("Add team member to the company: TAF_TEST_USER_TEAM_MEMBER");
+        step("Make a company inactive: " + "TAF_COMPANY_1");
+        step("Log in as a user: TAF_TEST_USER_TEAM_MEMBER");
+        step("'Company deactivated' notice should be displayed", () -> {
+        });
+    }
 
 }
